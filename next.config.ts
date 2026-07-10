7db6915800a4abd2754ece919bd6b7647da5b646
Vamp-Niklaus <rakeshkumarjnv7364@gmail.com>
2026-07-11 03:11:21 +0530

    fix: use undefined for devIndicators instead of true

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProduction = process.env.IS_PRODUCTION === '1';
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.1.12'],
-  devIndicators: !isProduction,
+  devIndicators: isProduction ? false : undefined,
 };
 
 export default nextConfig;
